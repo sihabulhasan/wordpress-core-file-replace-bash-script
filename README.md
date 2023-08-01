e8e198d814226e5a4e68e921859aa920598ef739
sihabul <admin@sihabul.com>
2023-08-02 02:43:04 +0600

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # wordpress-core-file-replay-bash-script
 You can replace your WordPress core file using this bash script
+
+To use the script follow the steps below:
+1. Download the wordpress-core-file-replace.sh file and upload in a directory of your server.
+2. Change the permission using the command: chmod +x wordpress-core-file-replace.sh
+3. Run the script using the command: ./wordpress-core-file-replace.sh
+4. Now it will ask a direcotry name. Provide the exact location where wordpress is installed. Like: /home/username/public_html
+5. After providing the exact location, it will replace the wordpress core file automatically.
---
+++ wordpress-core-file-replace.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Ask for the directory name
+echo "Enter the directory name:"
+read directory_name
+
+# Check if the directory exists
+if [ ! -d "$directory_name" ]; then
+    echo "Directory not found."
+    exit 1
+fi
+
+# Move to the directory
+cd "$directory_name"
+
+# Delete WordPress files and directories without wp-content and wp-config.php
+rm -rf cgi-bin wp-admin wp-includes index.php license.txt readme.html wp-activate.php wp-blog-header.php wp-comments-post.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php
+
+# Download and extract the latest WordPress zip file
+wget -q -O "wordpress.zip" "https://wordpress.org/latest.zip"
+unzip -qo "wordpress.zip"
+rm "wordpress.zip"
+
+# Move all files and directories (except wp-content and wp-config-sample.php) to the previous directory
+cd wordpress
+
+# Create an array of files to exclude
+exclude_files=(wp-content wp-config-sample.php)
+
+# Loop through all files and directories
+for item in *; do
+    # Check if the item is not in the exclude list
+    if [[ ! " ${exclude_files[@]} " =~ " ${item} " ]]; then
+        mv "$item" ../
+    fi
+done
+
+cd ..
+
+# Remove the empty 'wordpress' directory
+rm -rf wordpress
+
+echo "Task completed successfully."
